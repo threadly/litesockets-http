d0c4606b99d25480226e1b0538250519eafa6e11
Mike Jensen <jent@threadly.org>
2017-06-20 15:16:31 -0600

    HTTPResponse: More tollerant response formats
    It seems the response phrase is optional.  Because of that this changes the code to not require it.
    I also am handling the condition where the response code is non-numeric with a more friendly response error

--- protocol/src/main/java/org/threadly/litesockets/protocols/http/response/HTTPResponseHeader.java
+++ protocol/src/main/java/org/threadly/litesockets/protocols/http/response/HTTPResponseHeader.java
@@ -5,28 +5,27 @@ import java.nio.ByteBuffer;
 import org.threadly.litesockets.protocols.http.shared.HTTPConstants;
 import org.threadly.litesockets.protocols.http.shared.HTTPResponseCode;
 
-
-
 /**
  * An Immutable object of the HTTP Response header.  Basically the first line in the Header of an HTTP response. 
  */
 public class HTTPResponseHeader {
-  private static final int REQUIRED_RESPONSE_ITEMS = 3;
+  private static final int MAX_RESPONSE_ITEMS = 3;
   private final String rawResponse;
   private final HTTPResponseCode hrc;
   private final String httpVersion;
   
   public HTTPResponseHeader(String stringResponse) {
     this.rawResponse = stringResponse.trim();
-    String[] tmp = stringResponse.trim().split(" ", REQUIRED_RESPONSE_ITEMS);
-    if(tmp.length != REQUIRED_RESPONSE_ITEMS) {
-      throw new IllegalArgumentException("Invalide Response Header! :"+stringResponse);
+    String[] tmp = stringResponse.trim().split(" ", MAX_RESPONSE_ITEMS);
+    try {
+      httpVersion = tmp[0].trim();
+      if(!httpVersion.equalsIgnoreCase(HTTPConstants.HTTP_VERSION_1_1) && !httpVersion.equalsIgnoreCase(HTTPConstants.HTTP_VERSION_1_0)) {
+        throw new IllegalArgumentException("Unknown HTTP Version!:"+httpVersion);
+      }
+      hrc = HTTPResponseCode.findResponseCode(Integer.parseInt(tmp[1].trim()));
+    } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+      throw new IllegalArgumentException("Invalid Response Header! :"+stringResponse);
     }
-    httpVersion = tmp[0].trim();
-    if(!httpVersion.equalsIgnoreCase(HTTPConstants.HTTP_VERSION_1_1) && !httpVersion.equalsIgnoreCase(HTTPConstants.HTTP_VERSION_1_0)) {
-      throw new IllegalArgumentException("Unknown HTTP Version!:"+httpVersion);
-    }
-    hrc = HTTPResponseCode.findResponseCode(Integer.parseInt(tmp[1].trim()));
   }
   
   public HTTPResponseHeader(HTTPResponseCode rCode, String httpVersion) {
